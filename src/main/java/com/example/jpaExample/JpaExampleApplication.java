37cc3a5ef2f530a2f30e36b63e33159208db0cf1
tifuseini <tifuseini@st.ug.edu.gh>
2023-01-10 18:05:03 +0000

    command runner line

--- src/main/java/com/example/jpaExample/JpaExampleApplication.java
+++ src/main/java/com/example/jpaExample/JpaExampleApplication.java
@@ -1,13 +1,30 @@
 package com.example.jpaExample;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 @SpringBootApplication
+@Slf4j
 public class JpaExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpaExampleApplication.class, args);
 	}
+
+	/** Command line Runner Implementation **/
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			log.info("Hello World");
+		};
+
+	}
+
 
 }
