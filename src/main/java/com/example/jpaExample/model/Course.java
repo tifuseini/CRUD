4d32fb2e830c651f2ed4f1d7b20a100df8a35fbd
tifuseini <tifuseini@st.ug.edu.gh>
2023-01-11 11:42:21 +0000

    POJO

--- src/main/java/com/example/jpaExample/model/Course.java
+++ src/main/java/com/example/jpaExample/model/Course.java
@@ -1,2 +1,28 @@
-package com.example.jpaExample.model;public class Course {
+package com.example.jpaExample.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "rating")
+    private int rating;
+
+    @Column(name = "description")
+    private String description;
+
 }
