f9628175b9ddb242209d3e219569b923af8bab55
tifuseini <tifuseini@st.ug.edu.gh>
2023-01-11 16:15:35 +0000

    Test

--- src/test/java/com/example/jpaExample/JpaExampleApplicationTests.java
+++ src/test/java/com/example/jpaExample/JpaExampleApplicationTests.java
@@ -1,10 +1,23 @@
 package com.example.jpaExample;
 
+import com.example.jpaExample.service.CourseService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+@RequiredArgsConstructor
 class JpaExampleApplicationTests {
+
+	private final CourseService courseService;
+
+	private final MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
