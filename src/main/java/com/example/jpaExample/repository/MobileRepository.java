21cb4a0e213ba0a8d0d5358bcb95fc39cae846c2
tifuseini <tifuseini@st.ug.edu.gh>
2023-01-11 09:31:17 +0000

    update

--- src/main/java/com/example/jpaExample/repository/MobileRepository.java
+++ src/main/java/com/example/jpaExample/repository/MobileRepository.java
@@ -2,6 +2,8 @@ package com.example.jpaExample.repository;
 
 import com.example.jpaExample.model.MobileSubscriber;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MobileRepository extends JpaRepository<MobileSubscriber,Long> {
 }
--- src/main/java/com/example/jpaExample/service/MobileSubscriberService.java
+++ src/main/java/com/example/jpaExample/service/MobileSubscriberService.java
@@ -1,4 +1,11 @@
 package com.example.jpaExample.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MobileSubscriberService {
+
+
 }
