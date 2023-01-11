736c191bed3bf058ed69e36af62b4fc4f43e5eca
tifuseini <tifuseini@st.ug.edu.gh>
2023-01-11 12:08:57 +0000

    course controller update

--- src/main/java/com/example/jpaExample/controller/CourseController.java
+++ src/main/java/com/example/jpaExample/controller/CourseController.java
@@ -1,9 +1,9 @@
 package com.example.jpaExample.controller;
 
+import com.example.jpaExample.model.Course;
 import com.example.jpaExample.service.CourseService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +11,41 @@ import org.springframework.web.bind.annotation.RestController;
 public class CourseController {
 
     private final CourseService courseService;
+
+    @PostMapping("/")
+    public Course createCourse(Course course) {
+        return courseService.createCourse(course);
+    }
+
+    @GetMapping("/")
+    public Iterable<Course> getAllCourses() {
+        return courseService.getAllCourses();
+    }
+
+    @GetMapping("/{id}")
+    public Course getCourseById(@PathVariable Long id) {
+        return courseService.getCourseById(id).get();
+    }
+
+    @GetMapping("/category/{name}")
+    public Iterable<Course> getCoursesByCategory(@PathVariable String name) {
+        return courseService.getCoursesByCategory(name);
+    }
+
+    @PutMapping("/{id}")
+    public void updateCourse(@PathVariable Long id, Course course) {
+        courseService.updateCourse(id, course);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCourseById(@PathVariable Long id) {
+        courseService.deleteCourseById(id);
+    }
+
+    @DeleteMapping("/")
+    public void deleteAllCourses() {
+        courseService.deleteAllCourses();
+    }
+
+
 }
