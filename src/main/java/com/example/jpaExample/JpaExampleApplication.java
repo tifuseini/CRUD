73e744e5badbcb669b50e8c6fda0ab1653668173
tifuseini <tifuseini@st.ug.edu.gh>
2023-01-12 10:56:09 +0000

    update

--- src/main/java/com/example/jpaExample/JpaExampleApplication.java
+++ src/main/java/com/example/jpaExample/JpaExampleApplication.java
@@ -3,15 +3,11 @@ package com.example.jpaExample;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 @Slf4j
